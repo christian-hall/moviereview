7b815ad5de78bf6ca368354b7d04cbb14a040955
christian-hall <christian.hall395@gmail.com>
2021-03-10 16:45:46 -0500

    added display class

--- src/objects/MovieReview.java
+++ src/objects/MovieReview.java
@@ -1,10 +1,16 @@
 package objects;
 
+import java.util.List;
+
 public class MovieReview {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	}
+	
+	private static String printMenu(String title, List<String> options) {
+		return "test";
 	}
 
 }
